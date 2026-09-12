1f0a28ed9ee99679d6ec8609403029b033a766a8
sokpanha107-stack <sokpanha107@gmail.com>
2026-09-12 18:01:11 +0700

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -18,3 +18,12 @@ export const TECHNICAL_TERMS = [
 ] as const;
 
 export type TechnicalTerm = typeof TECHNICAL_TERMS[number];
+
+// 👉 ថែមកូដផ្នែកនេះចូលដើម្បីឱ្យ Vercel ស្គាល់ឈ្មោះប្រេនដែលយើងបានហៅប្រើ
+export const SAVPD_CONSTANTS = {
+  BRAND: {
+    NAME: "SAVPD.io",
+    TRADEMARK: "SAVPD.io™",
+    STUDIO: "SAVPD.io™",
+  },
+};
